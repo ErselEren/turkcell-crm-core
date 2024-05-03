9261def2e69999ee03c9bc95d2194a2b41bc1a3e
Ersel Celal Eren <erseleren_@hotmail.com>
2024-05-03 22:55:55 +0300

    .

--- src/main/java/com/turkcell/pair3/core/configuration/BaseApplicationConfiguration.java
+++ src/main/java/com/turkcell/pair3/core/configuration/BaseApplicationConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class BaseApplicationConfiguration {
+
     @Bean
     public PasswordEncoder passwordEncoder()
     {
