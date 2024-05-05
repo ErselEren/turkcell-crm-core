2d2f5dad02176bf5077750c82350b6838ec1249e
Ersel Celal Eren <erseleren_@hotmail.com>
2024-05-05 18:00:38 +0300

    MessageService added, LocaleResolver added

--- src/main/java/com/turkcell/pair3/core/configuration/BaseApplicationConfiguration.java
+++ src/main/java/com/turkcell/pair3/core/configuration/BaseApplicationConfiguration.java
@@ -1,10 +1,14 @@
 package com.turkcell.pair3.core.configuration;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+import java.util.Locale;
 
 @Configuration
 public class BaseApplicationConfiguration {
@@ -13,5 +17,21 @@ public class BaseApplicationConfiguration {
     public PasswordEncoder passwordEncoder()
     {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public ResourceBundleMessageSource bundleMessageSource()
+    {
+        ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+        source.setBasename("messages");
+        return source;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver()
+    {
+        AcceptHeaderLocaleResolver acceptHeaderLocaleResolver = new AcceptHeaderLocaleResolver();
+        acceptHeaderLocaleResolver.setDefaultLocale(new Locale("en"));
+        return acceptHeaderLocaleResolver;
     }
 }
